fff4df0db05c2a302224e27c4a028fb12249b044
Jeferson Neres Farias <jeferson.neres@gmail.com>
2023-03-15 19:28:59 -0300

    Foto inicial

--- src/components/home.component.js
+++ src/components/home.component.js
@@ -1,26 +1,44 @@
 import React, { useState, useEffect } from "react";
-
+import axios from 'axios';
 
 export default function Home(props) {
 
+    const [data, setData] = useState({})
+    const [carregado, setCarregado] = useState(false)
+
+    useEffect(() => {
+        const requests =[
+          axios.get(process.env.REACT_APP_URL_SERVER + 'servico/')
+        .then(res => res = res.data)
+      ];
+    
+      Promise.all(requests)
+          .then(([objServico]) => {        
+            setData({
+              tabela: objServico
+          })
+            setCarregado(true);
+          }, (evt) => {
+              console.log(evt);        
+          })
+      }, []);
+
   return (<div className="form-wrapper home">
-    <p className="titulo">Bem vindo(a)  ao salão Nádia Beauty Hair e Designer</p>
-    <p className="subtitulo">Um prazer poder te atender</p>
+    {carregado && <div><p className="titulo">Bem vindo(a)  ao salão Nádia Beauty Hair e Designer</p>
+    
+    <div className="fotoHome"></div>
+    
+    {/* <p className="subtitulo">Um prazer poder te atender</p>
     <p className="conteudo">Acesse o menu e agende um horário conosco</p>
     <div className="servicos">
         <p className="conteudo">Nosso salão dispõe dos seguintes serviços:</p>
         <ul>
-            <li>Unhas pé e mão</li>
-            <li>Pintura de cabelo</li>
-            <li>Corte de cabelo</li>
-            <li>Morena Iluminada</li>
-            <li>Luzes</li>
-            <li>Spar da beleza</li>
-            <li>Depilação</li>
-            <li>Progressiva</li>
-            <li>Hidratação</li>
+            {data.tabela.map((obj) => 
+                <li>{obj.nome}</li>    
+            )}            
         </ul>
-    </div>
+    </div> */}</div>}
+    
 </div>)
 
 
